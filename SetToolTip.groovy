2e9ea82271b29020258722f85ce424a9a2f2c063
Mika Nokka <mika.nokka1@gmail.com>
2019-01-31 16:32:19 +0200

    Working example. Only for Edit/Create screens

--- SetToolTip.groovy
+++ SetToolTip.groovy
@@ -1,5 +1,8 @@
 // POC to see if Jira Drop Down list can have tool tip or some info texts
 // Thnx to AdaptaVista ScriptRunner suppport!
+//
+// This shows description only in Edit / Create screens when value of drop down list
+// is changed
 
 package com.onresolve.jira.groovy.doit5  // this script must be living under this tree starting from /scripts directory (<jira place>/scripts/com/onresolve/jira/groovy/doit5)
 import com.atlassian.jira.component.ComponentAccessor
@@ -27,25 +30,40 @@ void doit5() {	// just a method runtime system is calling (used in Behaviours co
 	
 	
 		def log = Logger.getLogger("ToolTipper")
-		log.setLevel(Level.INFO) // DEBUG INFO
+		log.setLevel(Level.DEBUG) // DEBUG INFO
 	
 		log.debug("---------- ToolTipper started ---------------------------------------")
 		
 		// Get a pointer to my select list custom field
-		def RiskListField = getFieldByName("Risk Number")
+		def RiskListField = getFieldByName("Risk Number2")
 		// Get the value from the select list field
 		def RiskListFieldVal = RiskListField.getValue().toString()
 
 		// if option A is selected set B description
-		if(RiskListFieldVal.toString() == "2"){
+		if(RiskListFieldVal.toString() == "1"){
+			def description = "<b>This is a description for option 1</b>"
+			RiskListField.setDescription(description)
+			log.debug("1")
+		}else if(RiskListFieldVal.toString() == "2"){
 			def description = "<b>This is a description for option 2</b>"
 			RiskListField.setDescription(description)
-		}else if(RiskListFieldVal.toString() == "3"){
-		def description = "<b>This is a description for option 3</b>"
-		RiskListField.setDescription(description)
+			log.debug("2")
 }
-
-
+		else if(RiskListFieldVal.toString() == "3"){
+			def description = "<b>This is a description for option 3</b>"
+			RiskListField.setDescription(description)
+			log.debug("3")
+}
+		else if(RiskListFieldVal.toString() == "4"){
+			def description = "<b>This is a description for option 4</b>"
+			RiskListField.setDescription(description)
+			log.debug("4")
+}
+		else if(RiskListFieldVal.toString() == "5"){
+			def description = "<b>This is a description for option 5</b>"
+			RiskListField.setDescription(description)
+			log.debug("5")
+}
 		log.debug("---------- ToolTipper stopped ---------------------------------------")
 	}
 }
